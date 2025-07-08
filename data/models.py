0464faf2d175c85c01977b746db1de6f79f8cadc
NotInvented <notinvented01@gmail.com>
2025-07-08 19:54:13 +0300

    Updated image model. Need to rewrite logic to use it.

--- data/models.py
+++ data/models.py
@@ -17,18 +17,19 @@ class ProcessingStatus(Enum):
 
 
 class Image(Base):
+    image_bytes: bytes
+    vision_response: AnnotateImageResponse
+    analysis_results: List[str]
+
+class ImageModel:
     __tablename__ = 'images'
-    __allow_unmapped__ = True
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     original_file_path = Column(String(512), nullable=False)
     optimized_file_path = Column(String(512), nullable=False)
-    image_bytes: bytes
     filename = Column(String(255), nullable=False)
     file_hash = Column(String(128), nullable=False)
-    vision_response: AnnotateImageResponse
     vision_json_path = Column(String(512), nullable=False, default="")
-    analysis_results: List[str]
     processed_at = Column(DateTime, nullable=True)
     status = Column(SQLEnum(ProcessingStatus), nullable=False, default=ProcessingStatus.PENDING)
 
